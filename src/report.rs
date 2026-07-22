d1cfa21e161f7367e1b6ccf11697c24a0494f731
sphereflow <johannprescher@gmx.de>
2026-07-22 12:57:41 +0200

    Remove unused trait

--- src/report.rs
+++ src/report.rs
@@ -1,17 +1,6 @@
 use zerocopy::{FromBytes, Immutable, IntoBytes};
 
-#[allow(dead_code)]
-trait ToReport
-where
-    Self: Sized,
-{
-    fn to_report<const ID: u8>(self) -> Report<Self, ID> {
-        Report { id: ID, base: self }
-    }
-}
-
 #[repr(C, packed)]
-#[allow(dead_code)]
 #[derive(FromBytes, IntoBytes, Immutable, Debug, Clone, Copy, Default)]
 pub struct Report<T, const ID: u8> {
     id: u8,
